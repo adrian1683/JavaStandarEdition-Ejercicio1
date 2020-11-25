51e51dc73cf04fe9e991c219af2ceb1700091223
Adrian <adrian.quintero.monterrosa@gmail.com>
2020-11-25 12:23:54 -0500

    Agregando componentes al repositorio

---
+++ src/clases/CapacidadEndedudamiento.java
@@ -0,0 +1,43 @@
+package clases;
+
+public class CapacidadEndedudamiento {
+    Integer ingresosTotales;
+    Integer gastosFijos;
+    Integer gastoVaribales;
+    final double POR_FIJO = 0.35;
+
+    public Integer getIngresosTotales() {
+        return ingresosTotales;
+    }
+
+    public void setIngresosTotales(Integer ingresosTotales) {
+        this.ingresosTotales = ingresosTotales;
+    }
+
+    public Integer getGastosFijos() {
+        return gastosFijos;
+    }
+
+    public void setGastosFijos(Integer gastosFijos) {
+        this.gastosFijos = gastosFijos;
+    }
+
+    public Integer getGastoVaribales() {
+        return gastoVaribales;
+    }
+
+    public void setGastoVaribales(Integer gastoVaribales) {
+        this.gastoVaribales = gastoVaribales;
+    }
+
+    //Construye un metodo que retorne una cadena con las propiedades de la clase
+    public String mensaje(String informacion){
+
+        return "";
+    }
+
+    public String getCapacidadEndeudamiento() {
+        // retornar la capacidad de endeudamiento puede ser una cadena con el valor de la deuda
+        return "";
+    }
+}
